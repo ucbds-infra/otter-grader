078cbca6474f6a2f2cdb56af2f1f0c2fb74c1477
Chris Pyles <cpyles@berkeley.edu>
2019-09-20 16:20:29 -0700

    added groups to GradescopeParser

--- local_grader/metadata.py
+++ local_grader/metadata.py
@@ -13,21 +13,32 @@ from utils import *
 class GradescopeParser:
 	"""Metadata parser for Gradescope exports"""
 
-	def __init__(self, submissions_dir):
+	def __init__(self, submissions_dir, group=False):
 		# open metadata file and load into Python object
 		with open(os.path.join(submissions_dir, "submission_metadata.yml")) as f:
 			metadata = yaml.safe_load(f)
 
 		# initialize metadata list
 		self._metadata = []
-		for file in metadata:
-			self._metadata += [{
+		if not group:
+			for file in metadata:
+				self._metadata += [{
 
-				# metadata is separated by filename into a list of submitters
-				# and we use the SID as the identifier
-				"identifier": metadata[file][":submitters"][0][":sid"],
-				"filename": file
-			}]
+					# metadata is separated by filename into a list of submitters
+					# and we use the SID as the identifier
+					"identifier": metadata[file][":submitters"][0][":sid"],
+					"filename": file
+				}]
+		else:
+			for file in metadata:
+				for submitter in metadata[file][":submitters"]:
+					self._metadata += [{
+
+						# metadata is separated by filename into a list of submitters
+						# and we use the SID as the identifier
+						"identifier": submitter[":sid"],
+						"filename": file
+					}]
 
 	def get_metadata(self):
 		"""Returns mapping of identifiers to files"""
