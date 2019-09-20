e6bd959c1acb36eeeea43cb15e71b911420acfb5
Chris Pyles <cpyles@berkeley.edu>
2019-09-20 15:46:24 -0700

    removed python files in /

--- utils.py
+++
@@ -1,6 +0,0 @@
-import os
-
-def list_files(path):
-	"""Returns list of all files in a directory, ignores hidden files"""
-	assert os.path.isdir(path), "Provided path is not a directory"
-	return [file for file in os.listdir(path) if os.path.isfile(os.path.join(path, file)) and file[0] != "."]
