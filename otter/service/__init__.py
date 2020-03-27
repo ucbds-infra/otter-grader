03e9869862f0309f889847962abb5183d78b9d7f
Chris Pyles <cpyles@berkeley.edu>
2020-03-26 21:41:02 -0700

    added header

--- otter/service/__init__.py
+++ otter/service/__init__.py
@@ -1,3 +1,7 @@
+################################
+##### otter service Script #####
+################################
+
 from .create import main as create
 from .start import main as start
 from .update import main as update
