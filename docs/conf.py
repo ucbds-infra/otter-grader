5c1ed00993a3ee973fb0ed4a3c8cb3dc48b3897f
Chris Pyles <cpyles@berkeley.edu>
2020-04-02 15:17:25 -0700

    updated conf.py in docs

--- docs/conf.py
+++ docs/conf.py
@@ -96,20 +96,7 @@ pygments_style = 'sphinx'
 html_theme = 'pydata_sphinx_theme'
 
 html_theme_options = {
-    'canonical_url': '',
-    # 'analytics_id': 'UA-XXXXXXX-1',  #  Provided by Google in your dashboard
-    'logo_only': False,
-    'display_version': True,
-    'prev_next_buttons_location': 'bottom',
-    'style_external_links': False,
-    'vcs_pageview_mode': '',
-    'style_nav_header_background': 'white',
-    # Toc options
-    'collapse_navigation': True,
-    'sticky_navigation': True,
-    'navigation_depth': 2,
-    'includehidden': True,
-    'titles_only': False,
+    'github_url': 'https://github.com/ucbds-infra/otter-grader'
 }
 
 # Theme options are theme-specific and customize the look and feel of a theme
