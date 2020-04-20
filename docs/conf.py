3a87418a653d2e922a5028df48c454798e1219d5
Chris Holdgraf <choldgraf@berkeley.edu>
2020-04-20 11:30:03 -0700

    using sphinx book theme

--- docs/conf.py
+++ docs/conf.py
@@ -93,10 +93,11 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'pydata_sphinx_theme'
+html_theme = 'sphinx_book_theme'
 
 html_theme_options = {
-    'github_url': 'https://github.com/ucbds-infra/otter-grader'
+    'github_url': 'https://github.com/ucbds-infra/otter-grader',
+    'repository_url': 'https://github.com/ucbds-infra/otter-grader'
 }
 
 # Theme options are theme-specific and customize the look and feel of a theme
