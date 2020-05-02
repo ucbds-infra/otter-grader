e57e3c8b2cd7ed29c607656861372017f0b0a7bb
Abhishek Sharma <abhisheks@berkeley.edu>
2020-05-01 22:48:49 -0700

    Tests for create/build

---
+++ test/test-build/tests/q211.py
@@ -0,0 +1,25 @@
+test = {
+  'name': 'q211',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> # Hint: You can write the sine of 1.5*pi as:;
+          >>> #   math.sin(1.5 * math.pi);
+          >>> import math;
+          >>> round(sine_of_pi_over_four, 8)
+          0.70710678
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
