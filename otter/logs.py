985f9be47fdbe18206d16f400448e112e3a5ae4f
Chris Pyles <cpyles@berkeley.edu>
2020-05-24 14:00:29 -0700

    updated Log.__repr__

--- otter/logs.py
+++ otter/logs.py
@@ -92,7 +92,7 @@ class Log:
         self.ascending = ascending
 
     def __repr__(self):
-        return "otter.logs.Log([{}])".format(",\n".join([repr(e) for e in self.entries]))
+        return "otter.logs.Log([\n\t{}\n])".format(",\n\t".join([repr(e) for e in self.entries]))
 
     @classmethod
     def from_file(cls, filename, ascending=True):
