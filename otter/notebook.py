8df3afff6231572fe49ef89f0024824c54e8b4ff
Chris Pyles <cpyles@berkeley.edu>
2020-05-28 09:46:54 -0700

    changed tempfile

--- otter/notebook.py
+++ otter/notebook.py
@@ -129,16 +129,16 @@ class Notebook:
 		if global_env is None:
 			global_env = inspect.currentframe().f_back.f_globals
 		unshelved = []
-		_, tf_path = tempfile.mkstemp()
-		shelf = {}
-		for k, v in global_env.items():
-			try:
-				shelf[k] = v
-			except:
-				unshelved.append(k)
-		with open(tf_path, "wb") as tf:
-			pickle.dump(shelf, tf)
-			shelf_contents = tf.read()
+		with shelve.open(".OTTER_ENV") as shelf:
+			for k, v in global_env.items():
+				try:
+					shelf[k] = v
+				except:
+					unshelved.append(k)
+		tf = open(".OTTER_ENV", "rb")
+		shelf_contents = tf.read()
+		tf.close()
+		# os.rm(".OTTER_ENV")
 		return shelf_contents, unshelved
 
 		
