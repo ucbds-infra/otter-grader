7b9bca7724a629e16c357f7ad4530b30f40510aa
Chris Pyles <cpyles@berkeley.edu>
2020-05-31 17:07:20 -0700

    updated test regex

--- otter/assign.py
+++ otter/assign.py
@@ -33,7 +33,7 @@ TEST_HEADERS = ["TEST", "HIDDEN TEST"]
 ALLOWED_NAME = re.compile(r'[A-Za-z][A-Za-z0-9_]*')
 NB_VERSION = 4
 
-TEST_REGEX = r"##\s*(hidden\s*)?test\s*##"
+TEST_REGEX = r"(##\s*(hidden\s*)?test\s*##|#\s*(hidden\s*)?test)"
 SOLUTION_REGEX = r"##\s*solution\s*##"
 MD_SOLUTION_REGEX = r"(<strong>|\*{2})solution:?(<\/strong>|\*{2})"
 SEED_REGEX = r"##\s*seed\s*##"
