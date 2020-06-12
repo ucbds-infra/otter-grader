1d9093723ea056326773f6d84f3a56782d625bcb
alex yi <alexyi1234@berkeley.edu>
2020-06-11 19:51:27 -0700

    some logs nb and tex check

---
+++ test/test-logs/1.check-change/tests/q1.py
@@ -0,0 +1,23 @@
+test = {
+	"name": "q1",
+	"points": 0,
+	"hidden": False,
+	"suites": [ 
+		{
+			"cases": [ 
+				{
+					"code": r"""
+					>>> x
+					array([1, 2, 3, 4, 5])
+					""",
+					"hidden": False,
+					"locked": False,
+				}, 
+			],
+			"scored": False,
+			"setup": "",
+			"teardown": "",
+			"type": "doctest"
+		}
+	]
+}
