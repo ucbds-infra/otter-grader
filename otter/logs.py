72383c60204d269bc84fbf034cf173aa0795dadc
Chris Pyles <cpyles@berkeley.edu>
2020-06-18 21:46:07 -0700

    fixed verify_scores

--- otter/logs.py
+++ otter/logs.py
@@ -437,7 +437,7 @@ class Log:
         """
         Verifies scores in ``score_mapping`` (a ``dict`` of the structure returned by 
         ``otter.execute.grade_notebook``) against the results stored in this log using the results 
-        returned by ``Log.get_results`` for comparison. Raises a ``UserWarning`` if the scores differ
+        returned by ``Log.get_results`` for comparison. Prints a message if the scores differ
         by more than the default tolerance of ``numpy.isclose``.
 
         Args:
@@ -446,11 +446,6 @@ class Log:
 
         Returns:
             ``bool``: whether a discrepancy was found
-
-        Warns:
-            ``UserWarning``: if there is no entry in this log for a test in ``score_mapping``
-            ``UserWarning``: if the logged score for a test differs by more than the default tolerance
-                of ``numpy.isclose``
         """
         found_discrepancy = False
         for test in score_mapping:
@@ -460,9 +455,9 @@ class Log:
             try:
                 result = self.get_results(test)
                 grade = result.grade * result.tests[0].value
-                if not np.isclose(score, result.grade):
+                if not np.isclose(score, grade):
                     print("Score for {} ({:.3f}) differs from logged score ({:.3f})".format(
-                        test, score, result.grade
+                        test, score, grade
                     ))
                     found_discrepancy = True
             except QuestionNotInLogException:
