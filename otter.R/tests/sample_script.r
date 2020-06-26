391580523a894fd4fb37b141cd6964582468f0f6
Chris Pyles <cpyles@berkeley.edu>
2020-06-26 10:22:49 -0700

    updated example

--- otter.R/tests/sample_script.r
+++ otter.R/tests/sample_script.r
@@ -1,6 +1,14 @@
-setwd("/doesnotexist")
 
-a = 1
-b = 2
 
-. = otter::check("/Users/chrispyles/GitHub/otter-grader/otter.R/tests/sample_test.R")#, env)
+
+df = data.frame(a=c(1:5), b=c(2:6), c=c(7:11))
+
+df$a2 = df$a ^ 2
+
+. = otter::check("./tests/sample_test.R")
+
+
+
+
+
+
--- otter.R/tests/sample_test.r
+++ otter.R/tests/sample_test.r
@@ -4,30 +4,20 @@ test_metadata = "
 name: q1
 cases:
   - name: q1a
-    hidden: true
+    hidden: false
     points: 1
   - name: q1b
-    hidden: false
-    points: 1
-  - name: q1c
-    hidden: false
+    hidden: true
     points: 1
 "
 
 test_that("q1a", {
-  expect_equal(a, 1)
-  b = 2
-  expect_equal(a + b, 3)
+  expect_equal(df$a, c(1:5))
+  b = rep(2, 5)
+  expect_equal(df$a + b, c(3:7))
 })
 
 test_that("q1b", {
-  expect_equal(b, 2)
-  hidden = TRUE
-  expect_false(hidden)
+  expect_equal(df$a2, c(1:5)^2)
 })
 
-test_that("q1c", {
-  expect_equal(b, 2)
-  hidden = TRUE
-  expect_false(hidden)
-})
