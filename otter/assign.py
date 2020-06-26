69e497819ed72c6f6db893809bc8e280779bed2f
alex yi <alexyi1234@berkeley.edu>
2020-06-25 22:10:15 -0700

    clear

--- otter/assign.py
+++ otter/assign.py
@@ -49,6 +49,10 @@ ASSIGNMENT_METADATA = {}
 class EmptyCellException(Exception):
     """Exception for empty cells to indicate deletion"""
 
+def _clear_assignment_metadata():
+    """Replaces ASSIGNMENT_METADATA with an empty dictionary for testing"""
+    global ASSIGNMENT_METADATA
+    ASSIGNMENT_METADATA = {}
 
 def run_tests(nb_path, debug=False, seed=None):
     """Run tests in the autograder version of the notebook.
