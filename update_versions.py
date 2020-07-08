bb0dad0a75c7eb538774fba2e1940f9d84db7bf6
Chris Pyles <cpyles@berkeley.edu>
2020-07-08 11:34:57 -0700

    update versions

--- update_versions.py
+++ update_versions.py
@@ -9,8 +9,6 @@
 # CURRENT_VERSION: current version of the package (what to change)
 # NEW_VERSION: new version of the package (what to change it to)
 # FILES_WITH_VERSIONS: list of files that need to be updated
-# BETA: whether the new release is the current beta version
-# UNDO_BETA: whether we're undoing a beta release
 
 import re
 import subprocess
@@ -19,114 +17,56 @@ import warnings
 CURRENT_VERSION = "1.0.0"
 NEW_VERSION = "1.0.0.b1"
 
-FROM_BETA = "b" in CURRENT_VERSION.split(".")[-1]
-TO_BETA = "b" in NEW_VERSION.split(".")[-1]
+from_beta = "b" in CURRENT_VERSION.split(".")[-1]
+to_beta = "b" in NEW_VERSION.split(".")[-1]
 
 FILES_WITH_VERSIONS = [        # do not include setup.py
     "Dockerfile",
     "otter/generate/autograder.py",
     "test/test_generate/test-autograder/autograder-correct/requirements.txt",
-    # "requirements.txt",
-    # "Makefile"
 ]
 
 def main():
-    # if TO_GIT and subprocess.run(["git", "diff"], stdout=subprocess.PIPE).stdout.decode("utf-8").strip():
-    #     warnings.warn(
-    #         "You have uncommitted changes that will not be included in this release. To include "
-    #         "them, commit your changes and rerun this script.",
-    #         UserWarning
-    #     )
+    old_version = fr"otter-grader=={CURRENT_VERSION}$"
+    new_version = f"otter-grader=={NEW_VERSION}"
 
     for file in FILES_WITH_VERSIONS:
-        with open(file) as f:
+        with open(file, "r+") as f:
             contents = f.read()
+            f.seek(0)
+            contents = re.sub(
+                old_version, 
+                new_version, 
+                contents,
+                flags=re.MULTILINE
+            )
+            f.write(contents)
 
-        old_version = "otter-grader=={}".format(CURRENT_VERSION)
-        # if FROM_GIT:
-        #     old_version = r"git\+https:\/\/github\.com\/ucbds-infra\/otter-grader\.git@\w+"
-        
-        new_version = "otter-grader=={}".format(NEW_VERSION)
-        # if TO_GIT:
-        #     new_hash = (
-        #         subprocess
-        #         .run(["git", "rev-parse", "HEAD"], stdout=subprocess.PIPE)
-        #         .stdout
-        #         .decode("utf-8")
-        #         .strip()
-        #     )
-        #     new_version = f"git+https://github.com/ucbds-infra/otter-grader.git@{new_hash}"
+    if from_beta:
+        # fix Makefile
+        with open("Makefile", "r+") as f:
+            contents = f.read()
+            f.seek(0)
+            contents = re.sub("ucbdsinfra/otter-grader:beta", "ucbdsinfra/otter-grader", contents, flags=re.MULTILINE)
+            f.write(contents)
 
+    if to_beta:
+        # fix Makefile
+        with open("Makefile", "r+") as f:
+            contents = f.read()
+            f.seek(0)
+            contents = re.sub(r"ucbdsinfra/otter-grader$", "ucbdsinfra/otter-grader:beta", contents, flags=re.MULTILINE)
+            f.write(contents)
+
+    # fix otter.__version__
+    with open("otter/version.py", "r+") as f:
+        contents = f.read()
+        f.seek(0)
         contents = re.sub(
-            old_version, 
-            new_version, 
+        fr"__version__ = ['\"]{CURRENT_VERSION}['\"]",
+            f"__version__ = \"{NEW_VERSION}\"",
             contents
         )
-
-        with open(file, "w") as f:
-            f.write(contents)
-
-    if FROM_BETA:
-        # # fix documentation
-        # with open("docs/conf.py") as f:
-        #     contents = f.read()
-
-        # contents = re.sub("master_doc = 'index_beta'", "master_doc = 'index'", contents)
-
-        # with open("docs/conf.py", "w") as f:
-        #     f.write(contents)
-
-        # fix Makefile
-        with open("Makefile") as f:
-            contents = f.read()
-
-        contents = re.sub("ucbdsinfra/otter-grader:beta", "ucbdsinfra/otter-grader", contents)
-
-        with open("Makefile", "w") as f:
-            f.write(contents)
-
-    if TO_BETA:
-        # # fix documentation
-        # with open("docs/conf.py") as f:
-        #     contents = f.read()
-
-        # contents = re.sub("master_doc = 'index'", "master_doc = 'index_beta'", contents)
-
-        # with open("docs/conf.py", "w") as f:
-        #     f.write(contents)
-
-        # fix Makefile
-        with open("Makefile") as f:
-            contents = f.read()
-
-        contents = re.sub(r"ucbdsinfra/otter-grader", "ucbdsinfra/otter-grader:beta", contents)
-
-        with open("Makefile", "w") as f:
-            f.write(contents)
-    
-    # # else:
-    # with open("setup.py") as f:
-    #     contents = f.read()
-
-    # contents = re.sub(
-    #     "version = \"{}\",".format(CURRENT_VERSION),
-    #     "version = \"{}\",".format(NEW_VERSION),
-    #     contents
-    # )
-
-    # with open("setup.py", "w") as f:
-    #     f.write(contents)
-
-    with open("otter/version.py") as f:
-        contents = f.read()
-
-    contents = re.sub(
-       fr"__version__ = ['\"]{CURRENT_VERSION}['\"]",
-        f"__version__ = \"{NEW_VERSION}\"",
-        contents
-    )
-
-    with open("otter/version.py", "w") as f:
         f.write(contents)
 
     print(f"Versions updated. Release version is {NEW_VERSION} -- run 'make distro' to release.")
