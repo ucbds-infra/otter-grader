f28bb28bc4a9cea0399155a65409faef972bb5f5
Chris Pyles <cpyles@berkeley.edu>
2020-07-22 19:31:28 -0700

    started test logs

--- test/test_logs.py
+++ test/test_logs.py
@@ -2,7 +2,11 @@
 ##### Tests for Otter Logs #####
 ################################
 
+import os
 import unittest
+
+from otter.check.logs import Log
+from otter.check.notebook import Notebook, _OTTER_LOG_FILENAME
 
 from . import TestCase
 
@@ -18,5 +22,34 @@ TEST_FILES_PATH = "test/test-logs/"
 
 class TestLogs(TestCase):
 
+    grading_results = {}
+
+    def setUp(self):
+        super().setUp()
+
+        test_files_path = TEST_FILES_PATH + "tests"
+
+        grader = Notebook(test_files_path)
+
+        def square(x):
+            return x**2
+
+        for test_file in os.listdir(test_files_path):
+            if os.path.splitext(test_file)[1] != ".py":
+                continue
+            test_name = os.path.splitext(test_file)[0]
+            self.grading_results[test_name] = grader.check(test_name)
+
     def test_something(self):
-        pass
+        log = Log.from_file(_OTTER_LOG_FILENAME)
+
+        for question in log.get_questions():
+            logged_result = log.get_results(question)
+            actual_result = self.grading_results[question]
+
+            # checking repr since the results __eq__ method is not defined
+            self.assertEqual(repr(logged_result), repr(actual_result), f"Logged results for {question} are not correct")
+
+    def tearDown(self):
+        if os.path.isfile(_OTTER_LOG_FILENAME):
+            os.remove(_OTTER_LOG_FILENAME)
