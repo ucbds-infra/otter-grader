060fd1777ecf9ee245003ed7e90dcca21330fadd
Chris Pyles <cpyles@berkeley.edu>
2020-07-28 19:08:53 -0700

    fixed test

--- test/test_logs.py
+++ test/test_logs.py
@@ -68,8 +68,8 @@ class TestLogs(TestCase):
         log = Log.from_file(_OTTER_LOG_FILENAME)
 
         for question in log.get_questions():
-            logged_grade = log.get_question_entry(question).get_results()
-            actual_grade = self.grading_results[question]
+            logged_grade = log.get_question_entry(question).get_score_perc()
+            actual_grade = self.grading_results[question].grade
 
             # checking repr since the results __eq__ method is not defined
             self.assertEqual(repr(logged_grade), repr(actual_grade), f"Logged results for {question} are not correct")
