db814f6f61816adb6cd60fba35e3309ad1f57ebb
Chris Pyles <cpyles@berkeley.edu>
2020-10-02 18:37:24 -0700

    docstring

--- otter/plugins/builtin/grade_override.py
+++ otter/plugins/builtin/grade_override.py
@@ -17,7 +17,17 @@ class GoogleSheetsGradeOverride(AbstractOtterPlugin):
     test case scores by matching on the Gradescope assignment ID, student email, and test case name.
 
     Implements the ``during_generate`` and ``after_grading`` events. For plugin configurations, use
-    key ``google_sheets_grade_override``.
+    key ``google_sheets_grade_override``. 
+    
+    The google sheet should have the following format:
+
+    | Assignment ID | Email            | Test Case | Points |
+    |---------------|------------------|-----------|--------|
+    | 123456        | student@univ.edu | q1a - 1   | 1      |
+
+    ``Assignment ID`` should be the ID of the assignment on Gradescope, ``Email`` should be the email
+    address corresponding to the student's Gradescope account, ``Test Case`` should be the name of
+    the test case, and ``Points`` should be the number of points that the student should be assigned.
     """
 
     PLUGIN_CONFIG_KEY = "google_sheets_grade_override"
@@ -39,11 +49,11 @@ class GoogleSheetsGradeOverride(AbstractOtterPlugin):
         df = pd.DataFrame(data, columns=colnames)
         return df
 
-    # | Assignment ID | Email            | Test Case | Points |
-    # |---------------|------------------|-----------|--------|
-    # | 123456        | student@univ.edu | q1a - 1   | 1      |
-
     def after_grading(self, results):
+        """
+        Modifies the results of grading by pulling in the Google Sheet as a dataframe and updating 
+        the scores for the test cases found.
+        """
         df = self._load_df()
         df = df[
             df["Email"].isin([user["email"] for user in self.submission_metadata["users"]]) & \
@@ -53,6 +63,11 @@ class GoogleSheetsGradeOverride(AbstractOtterPlugin):
             results.update_result(row["Test Case"], score=row["Points"])
 
     def during_generate(self, otter_config):
+        """
+        Takes a path to Google Service Account credentials stored in this plugin's config as key
+        ``credentials_json_path`` and extracts the data from that file into the plugin's config as key
+        ``service_account_credentials``.
+        """
         creds_path = otter_config["plugin_config"][self.PLUGIN_CONFIG_KEY]["credentials_json_path"]
         with open(creds_path) as f:
             creds = json.load(f)
