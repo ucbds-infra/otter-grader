a3a55a1a83765da3332950d1a60d5672e0a85819
Chris Pyles <cpyles@berkeley.edu>
2021-01-09 00:17:02 -0800

    added seed for R

--- otter/run/run_autograder/r_adapter/run_autograder.py
+++ otter/run/run_autograder/r_adapter/run_autograder.py
@@ -5,12 +5,25 @@ Gradescope autograding internals for R
 import os
 import json
 import shutil
+import nbformat
 import jupytext
 
 from glob import glob
 from rpy2.robjects import r
 
 from ..constants import DEFAULT_OPTIONS
+from ..utils import get_source
+
+NBFORMAT_VERSION = 4
+
+def insert_seeds(nb_path, seed):
+    nb = nbformat.read(nb_path, as_version=NBFORMAT_VERSION)
+    for i, cell in enumerate(nb['cells']):
+        if cell['cell_type'] == 'code':
+            source = get_source(cell)
+            source = [f"set.seed({seed})"] + source
+            cell['source'] = "\n".join(source)
+    nbformat.write(nb, nb_path)
 
 def run_autograder(options):
     """
@@ -35,6 +48,9 @@ def run_autograder(options):
     # convert ipynb files to Rmd files
     if glob("*.ipynb"):
         fp = glob("*.ipynb")[0]
+        if options["seed"] is not None:
+            assert isinstance(options["seed"], int), f"{options['seed']} is an invalid seed"
+            insert_seeds(fp, options["seed"])
         nb = jupytext.read(fp)
         jupytext.write(nb, os.path.splitext(fp)[0] + ".Rmd")
     
