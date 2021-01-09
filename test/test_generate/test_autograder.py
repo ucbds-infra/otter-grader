a69cb2e745b070a0e348bba26b94e14cf25e5706
Chris Pyles <cpyles@berkeley.edu>
2021-01-09 10:47:17 -0800

    fixed generate test

--- test/test_generate/test_autograder.py
+++ test/test_generate/test_autograder.py
@@ -11,12 +11,13 @@ from subprocess import PIPE
 from glob import glob
 from unittest import mock
 
-from otter.argparser import get_parser
+# from otter.argparser import get_parser
 from otter.generate.autograder import main as autograder
+from otter.runner import run_otter
 
 from .. import TestCase
 
-parser = get_parser()
+# parser = get_parser()
 
 TEST_FILES_PATH = "test/test_generate/test-autograder/"
 
@@ -28,15 +29,16 @@ class TestAutograder(TestCase):
         """
         # create the zipfile
         generate_command = [
-            "generate", "autograder",
+            "generate",
             "-t", TEST_FILES_PATH + "tests",
             "-o", TEST_FILES_PATH,
             "-r", TEST_FILES_PATH + "requirements.txt",
             TEST_FILES_PATH + "data/test-df.csv"
         ]
-        args = parser.parse_args(generate_command)
-        args.func = autograder
-        args.func(args)
+        # args = parser.parse_args(generate_command)
+        # args.func = autograder
+        # args.func(args)
+        run_otter(generate_command)
 
         with self.unzip_to_temp(TEST_FILES_PATH + "autograder.zip", delete=True) as unzipped_dir:
             self.assertDirsEqual(unzipped_dir, TEST_FILES_PATH + "autograder-correct")
