7cb8fd57028e7c5cb869182bffaa2edc734ab6c5
Chris Pyles <cpyles@berkeley.edu>
2021-08-10 16:58:44 -0700

    unsaved file from merge

--- otter/run/run_autograder/constants.py
+++ otter/run/run_autograder/constants.py
@@ -2,7 +2,6 @@
 Default configurations for autograding
 """
 
-<<<<<<< HEAD
 from ...utils import convert_config_description_dict
 
 # a dictionary for documenting each configuration and its default value; the dictionary of actual
@@ -126,33 +125,3 @@ DEFAULT_OPTIONS_WITH_DESCRIPTIONS = [
 ]
 
 DEFAULT_OPTIONS = convert_config_description_dict(DEFAULT_OPTIONS_WITH_DESCRIPTIONS)
-=======
-DEFAULT_OPTIONS = {
-    "score_threshold": None,
-    "points_possible": None,
-    "show_stdout": False,
-    "show_hidden": False,
-    "show_all_public": False,
-    "seed": None,
-    "grade_from_log": False,
-    "serialized_variables": {},
-    "public_multiplier": 0,
-    "pdf": False,
-    "token": None,
-    "course_id": 'None',
-    "assignment_id": 'None',
-    "filtering": False,
-    "pagebreaks": False,
-    "debug": False,
-    "autograder_dir": '/autograder',
-    "lang": 'python',
-    "miniconda_path": '/root/miniconda3',
-    "test_visibility": 'hidden',
-    "separate_tests": True,
-    "plugins": [],
-    "logo": True,
-    "print_summary": True,
-    "print_score": True,
-    "zips": False,
-}
->>>>>>> a37ea19f0e054838ff551eecd9073973d5b677f4
