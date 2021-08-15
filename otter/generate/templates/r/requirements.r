ba22bbe9dac428919af907a612f326a939e1a488
Chris Pyles <cpyles@berkeley.edu>
2021-08-15 12:15:38 -0700

    fixing stringi build

--- otter/generate/templates/r/requirements.r
+++ otter/generate/templates/r/requirements.r
@@ -6,7 +6,11 @@ install.packages(c(
     "startup"
 ), dependencies=TRUE, repos="http://cran.us.r-project.org")
 
-install.packages("stringi", configure.args="--configure-args='--disable-pkg-config --with-extra-cxxflags="--std=c++11" --disable-cxx11'", repos="http://cran.us.r-project.org")
+install.packages(
+    "stringi", 
+    configure.args='--disable-pkg-config --with-extra-cxxflags="--std=c++11" --disable-cxx11', 
+    repos="http://cran.us.r-project.org"
+)
 {% endif %}{% if other_requirements %}
 {{ other_requirements }}
 {% endif %}
--- test/test-assign/rmd-autograder-correct/requirements.r
+++ test/test-assign/rmd-autograder-correct/requirements.r
@@ -6,4 +6,8 @@ install.packages(c(
     "startup"
 ), dependencies=TRUE, repos="http://cran.us.r-project.org")
 
-install.packages("stringi", configure.args="--configure-args='--disable-pkg-config --with-extra-cxxflags="--std=c++11" --disable-cxx11'", repos="http://cran.us.r-project.org")
+install.packages(
+    "stringi", 
+    configure.args='--disable-pkg-config --with-extra-cxxflags="--std=c++11" --disable-cxx11', 
+    repos="http://cran.us.r-project.org"
+)
