be4f75ad24523a9c84bb9d99453192c7f8a305ea
Chris Pyles <cpyles@berkeley.edu>
2021-09-02 23:01:55 -0700

    working on fixing parsing

--- otter/run/run_autograder/utils.py
+++ otter/run/run_autograder/utils.py
@@ -11,9 +11,10 @@ def add_quote_escapes(s):
     """
     Adds escape characters for quotes in a string.
     """
-    new_s = ""
+    new_s, last_c = "", ""
     for c in s:
-        if c == '"':
+        if c == '"' and last_c != "\\":
             new_s += "\\"
         new_s += c
+        last_c = c
     return new_s
