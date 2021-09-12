8c5782368acee664ad2116d0fda2bb5a11c782ef
Chris Pyles <cpyles@berkeley.edu>
2021-09-12 14:26:34 -0700

    fixed bug

--- otter/assign/utils.py
+++ otter/assign/utils.py
@@ -30,10 +30,10 @@ class AssignNotebookFormatException(Exception):
     """
     """
     def __init__(self, message, question_metadata, cell_index, *args, **kwargs):
-        question = question_metadata.get("name")
+        question_name = question_metadata.get("name")
         message += " ("
-        if question is not None:
-            message += f"question { question_metadata }, "
+        if question_name is not None:
+            message += f"question { question_name }, "
         message = message + f"cell number { cell_index + 1 })"
         super().__init__(message, *args, **kwargs)
 
