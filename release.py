7aca67c687b69276d19c366e875c2d750bf36159
Chris Pyles <cpyles@berkeley.edu>
2021-11-10 21:12:18 -0800

    removed release commands

--- release.py
+++ release.py
@@ -21,41 +21,6 @@ FILES_WITH_VERSIONS = [        # do not include setup.py, otter/version.py
 ]
 
 
-def run_release_commands(test, beta, new_version, no_twine=False):
-    assert shutil.which("hub") is not None, (
-        "You must have the GitHub CLI installed to use this script. Please see "
-        "https://github.com/github/hub to install it."
-    )
-
-    commands = [
-        "rm dist/* || :",
-        "python3 setup.py sdist bdist_wheel",
-        f"python3 -m twine upload dist/*{' --repository-url https://test.pypi.org/legacy/' if test else ''}",
-        "sleep 10", # this allows time for PyPI to update before we try to install the new version
-        f"docker build . -t ucbdsinfra/otter-grader:{new_version} --build-arg BUILDKIT_INLINE_CACHE=1",
-        f"docker push ucbdsinfra/otter-grader:{new_version}",
-        f"docker build . -t ucbdsinfra/otter-grader{':beta' if beta else ''} --build-arg BUILDKIT_INLINE_CACHE=1",
-        f"docker push ucbdsinfra/otter-grader{':beta' if beta else ''}",
-        "make tutorial",
-        f"git commit -am 'release v{new_version}'",
-        f"git push upstream master",
-        f"hub release create -a dist/*.tar.gz -a dist/*.whl -m 'v{new_version}' {' -p' if beta else ''} {new_version}",
-    ]
-
-    if not beta:
-        commands.append(f"gh pr create --base 'stable' --head 'master' --title 'Release v{new_version}' " \
-            f"--milestone 'v{new_version}' --project 'Otter-Grader Issues and Tasks' " \
-            f"--repo 'ucbds-infra/otter-grader' --body ''")
-
-    if no_twine:
-        del commands[2]
-
-    for cmd in commands:
-        subprocess.run(cmd, shell=True, check=True)
-
-    print("Releases pushed to PyPI, DockerHub, and GitHub. Commit and push to release documentation.")
-
-
 PARSER = argparse.ArgumentParser()
 PARSER.add_argument("new_version", nargs="?", default=None, help="Old version for regex search")
 PARSER.add_argument("--dry-run", action="store_true", default=False, help="Update files only but do not push release")
@@ -183,8 +148,3 @@ if __name__ == "__main__":
 
     else:
         print(f"Versions updated. Release version is {new_version_number}")
-
-    if args.dry_run:
-        sys.exit()
-        
-    run_release_commands(args.test, to_beta, new_version_number, no_twine=args.no_twine)
