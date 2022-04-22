653a3fa3581c2f2f3ca98d4a58cfe3b993f84329
Chris Pyles <cpyles@berkeley.edu>
2022-04-21 20:12:50 -0700

    add libmagick++-dev

--- otter/generate/templates/r/setup.sh
+++ otter/generate/templates/r/setup.sh
@@ -3,7 +3,7 @@
 if [ "${BASE_IMAGE}" != "ucbdsinfra/otter-grader" ]; then
     apt-get clean
     apt-get update
-    apt-get install -y pandoc texlive-xetex texlive-fonts-recommended texlive-generic-recommended build-essential libcurl4-gnutls-dev libxml2-dev libssl-dev libgit2-dev texlive-lang-chinese
+    apt-get install -y pandoc texlive-xetex texlive-fonts-recommended texlive-generic-recommended build-essential libcurl4-gnutls-dev libxml2-dev libssl-dev libgit2-dev texlive-lang-chinese libmagick++-dev
 
     # install wkhtmltopdf
     wget --quiet -O /tmp/wkhtmltopdf.deb https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.bionic_amd64.deb
