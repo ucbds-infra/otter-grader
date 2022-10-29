1c485216daaa7d40a3fd0058281696abb3d93659
Chris Pyles <cpyles@berkeley.edu>
2022-10-29 11:07:14 -0700

    removed integration test

--- test/test_integration/files/README.md
+++
@@ -1,6 +0,0 @@
-# Data 88E Project 3
-
-This is the Assign v1 formatted version of an assignment from [Data 88E at UC Berkeley](https://data-88e.github.io/).
-It is a slightly modified version of Project 3 from Spring 2020.
-
-This version of the notebook is used for Otter-Grader's integration tests.
--- test/test_integration/files/expected-autograder-zip/tests/q3.py
+++
@@ -1,9 +0,0 @@
-OK_FORMAT = True
-
-test = {   'name': 'q3',
-    'points': 3,
-    'suites': [   {   'cases': [{'code': '>>> assert len(my_labels) <= 10\n>>> assert "bill_sep05" not in my_labels\n', 'hidden': False, 'locked': False}],
-                      'scored': True,
-                      'setup': '',
-                      'teardown': '',
-                      'type': 'doctest'}]}
