882d32f31abd629c7822ae507a8a60b5f934aabd
Chris Pyles <cpyles@berkeley.edu>
2024-10-01 12:14:19 -0700

    switch from mambaforge to miniforge

--- test/test_generate/files/autograder-custom-env/setup.sh
+++ test/test_generate/files/autograder-custom-env/setup.sh
@@ -13,9 +13,9 @@ dpkg -i /tmp/pandoc.deb
 
 # install mamba
 if [ $(uname -p) = "arm" ] || [ $(uname -p) = "aarch64" ] ; \
-    then wget -nv https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-Linux-aarch64.sh \
+    then wget -nv https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-aarch64.sh \
         -O /autograder/source/mamba_install.sh ; \
-    else wget -nv https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-Linux-x86_64.sh \
+    else wget -nv https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh \
         -O /autograder/source/mamba_install.sh ; \
 fi
 chmod +x /autograder/source/mamba_install.sh
