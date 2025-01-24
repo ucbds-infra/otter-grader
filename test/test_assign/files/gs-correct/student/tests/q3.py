6e070256d2132e5744c0e80cbd3fa310734b58e5
Chris Pyles <40970945+chrispyles@users.noreply.github.com>
2025-01-24 11:22:38 -0800

    disallow metadata tests for colab notebooks (#913)

---
+++ test/test_assign/files/gs-correct/student/tests/q3.py
@@ -0,0 +1,9 @@
+OK_FORMAT = True
+
+test = {   'name': 'q3',
+    'points': None,
+    'suites': [   {   'cases': [{'code': '>>> nine\n9', 'hidden': False, 'locked': False}, {'code': '>>> square(16)\n256', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
